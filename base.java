21ce6c9e92ea9e2f89fccabc7689c3721dee37af
Mark <markaramirez8@gmail.com>
2015-02-12 05:14:46 +0000

    added comments

--- base.java
+++ base.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import static java.lang.System.out;
 
 public class base {
 
@@ -7,18 +8,19 @@ public class base {
 		Scanner scan = new Scanner(new File("file.dat"));
 		int n = scan.nextInt();
 		scan.nextLine();
-		//variables
-		for(int i = 0;i < n;i++)
+		// initialize variables to zero here
+		for(int i = 0;i < n;i++) //cycle through number of cases. if no n: while(scan.hasNext())
 		{
-			//read
+			//read with scanner methods
 			//scan.nextLine(), scan.nextInt(), scan.nextDouble()
-			//break up data, interpret
-			//logic, beef of the code
-			//print out output
+			//break up data, interpret using .trim() or .split()
+			//logic, beef of the code, conditions, algorithm
+			//print out output, formatted exactly as wanted
 			//set everything back to zero, clear everything
 		}
 	}
 }
+//3 Steps to Most/All Problems:
 //reading in data
 //interpreting or manipulating data
 //output
